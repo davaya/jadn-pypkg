ba9c5b34717fee80425ebb6060cb38fd35d58b70
davaya <19152940+davaya@users.noreply.github.com>
2020-06-11 13:43:42 -0400

    Add example messages

--- distribution/quickstart.py
+++ distribution/quickstart.py
@@ -9,7 +9,7 @@ from jadn.translate import json_schema_dumps
 """
 Define and validate a JADN schema
 """
-schema_data = {
+schema_data = {     # This is a Python literal, not JSON. It is a value as returned by json.load() or jadn.load()
     'types': [
         ['Person', 'Record', [], 'JADN equivalent of structure from https://developers.google.com/protocol-buffers', [
             [1, 'name', 'String', [], ''],
@@ -19,26 +19,31 @@ schema_data = {
     ]]
 }
 schema = jadn.check(schema_data)
-assert schema == schema_data            # jadn.check returns unmodified schema for chaining
+assert schema == schema_data            # jadn.check returns unmodified schema to facilitate chaining
 
 """
 Convert schema to alternate formats
 """
 print('\nSchema (Generic JSON):\n----------------------')
-print(json.dumps(schema, indent=1))     # Display schema as generic JSON data
+print(json.dumps(schema, indent=1))
+
 print('\nSchema (JADN-formatted JSON):\n-----------------------------')
-print(jadn.dumps(schema))               # Display schema as JADN-formatted JSON data
+print(jadn.dumps(schema))
+
 print('\nSchema (JADN IDL):\n------------------')
-print(jidl_dumps(schema))               # Display schema as IDL text
+print(jidl_dumps(schema))
+
 print('\nSchema (JADN IDL with truncated comments):\n------------------')
 print(jidl_dumps(strip_comments(schema, width=32)))
+
 print('\nSchema (HTML):\n------------------')
 print(html_dumps(schema))
-with open(os.path.join(jadn.data_dir(), 'dtheme.css')) as f:
-    print(' (Use with:', f.read(50), '...)')
+theme = os.path.join(jadn.data_dir(), 'dtheme.css')
+with open(theme) as f:
+    print(f' (Render with {theme}: {f.read(50)} ...)')
+
 print('\nSchema (Markdown):\n------------------')
 print(table_dumps(schema))
-
 
 print('\nSchema (JSON Schema):\n------------------')
 schema.update({                         # JSON Schema conversion needs a designated root type (Person)
@@ -53,16 +58,21 @@ print(json_schema_dumps(schema))
 Validate and serialize test data
 """
 def print_encoded_data(codec):
-    p1 = {'id': 14912, 'name': 'Joe'}                                   # Valid "Person" record
-    p2 = {'name': 'Joe', 'id': '123-45-6789'}                           # Bad: ID must be an Integer
-    p3 = {'name': 'Joe', 'email': 'joe@mailbox.com'}                    # Bad: ID is required
-    p4 = {'email': 'joe.mailbox.com', 'name': 'Joe', 'id': 14912}       # Bad: Not a valid email address
-    for name, data in {'p1': p1, 'p2': p2, 'p3': p3, 'p4': p4}.items():
+    data = [
+        {'id': 14912, 'name': 'Joe'},                                       # Valid "Person" record
+        {'name': 'Karen', 'id': 37145, 'email': 'karen@symphony.org'},      # Valid "Person" record
+        {'email': 'alf@hotmail.com', 'id': 20443, 'name': 'Lester'},        # Valid "Person" record
+        {'name': 'Joe', 'id': '123-45-6789'},                               # Bad: ID must be an Integer
+        {'name': 'Joe', 'email': 'joe@mailbox.com'},                        # Bad: ID is required
+        {'email': 'joe.mailbox.com', 'name': 'Joe', 'id': 14912},           # Bad: Not a valid email address
+    ]
+
+    for n, v in enumerate(data, start=1):
         try:
-            encoded_data = json.dumps(codec.encode('Person', data))     # Validate that data is an instance of Person
-            print(f'Valid {name}: {encoded_data}')
-        except (ValueError, TypeError) as err:                          # Validation errors raise an exception
-            print(f'Error {name}: {err}')
+            encoded_data = json.dumps(codec.encode('Person', v))    # Validate that data is an instance of Person
+            print(f'{n:>3} Valid: {encoded_data}')
+        except (ValueError, TypeError) as err:                      # Validation errors raise an exception
+            print(f'{n:>3} Error: {err}')
 
 
 print('\nSerialized Data:\n----------------')
