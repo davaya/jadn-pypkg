0854294567cb44e96f781f96e3301d4fe564be13
davaya <19152940+davaya@users.noreply.github.com>
2020-06-12 10:52:13 -0400

    Add json schema validation to quickstart

--- distribution/quickstart.py
+++ distribution/quickstart.py
@@ -1,6 +1,8 @@
 import json
-import jadn
+import jsonschema
 import os
+
+import jadn
 from jadn.codec import Codec
 from jadn.transform import strip_comments
 from jadn.convert import jidl_dumps, table_dumps, html_dumps
@@ -9,7 +11,7 @@ from jadn.translate import json_schema_dumps
 """
 Define and validate a JADN schema
 """
-schema_data = {     # This is a Python literal, not JSON. It is a value as returned by json.load() or jadn.load()
+schema_data = {     # This is a Python value as returned by json.loads(), not a JSON string.
     'types': [
         ['Person', 'Record', [], 'JADN equivalent of structure from https://developers.google.com/protocol-buffers', [
             [1, 'name', 'String', [], ''],
@@ -24,10 +26,7 @@ assert schema == schema_data            # jadn.check returns unmodified schema t
 """
 Convert schema to alternate formats
 """
-print('\nSchema (Generic JSON):\n----------------------')
-print(json.dumps(schema, indent=1))
-
-print('\nSchema (JADN-formatted JSON):\n-----------------------------')
+print('\nSchema:\n------------------')
 print(jadn.dumps(schema))
 
 print('\nSchema (JADN IDL):\n------------------')
@@ -48,11 +47,12 @@ print(table_dumps(schema))
 print('\nSchema (JSON Schema):\n------------------')
 schema.update({                         # JSON Schema conversion needs a designated root type (Person)
     'meta': {
-        'module': 'http://example.com/rolodex/v1.0',
+        'module': 'http://example.com/rolodex/v1.0',        # need a namespace $id
         'exports': ['Person']
     }
 })
-print(json_schema_dumps(schema))
+js_schema = json_schema_dumps(schema)
+print(js_schema)
 
 """
 Validate and serialize test data
@@ -64,7 +64,7 @@ def print_encoded_data(codec):
         {'email': 'alf@hotmail.com', 'id': 20443, 'name': 'Lester'},        # Valid "Person" record
         {'name': 'Joe', 'id': '123-45-6789'},                               # Bad: ID must be an Integer
         {'name': 'Joe', 'email': 'joe@mailbox.com'},                        # Bad: ID is required
-        {'email': 'joe.mailbox.com', 'name': 'Joe', 'id': 14912},           # Bad: Not a valid email address
+        {'email': '@joe.mailbox.com', 'name': 'Joe', 'id': 14912},          # Bad: Not a valid email address
     ]
 
     for n, v in enumerate(data, start=1):
@@ -73,6 +73,11 @@ def print_encoded_data(codec):
             print(f'{n:>3} Valid: {encoded_data}')
         except (ValueError, TypeError) as err:                      # Validation errors raise an exception
             print(f'{n:>3} Error: {err}')
+
+        try:                                                        # Validate same data using generated JSON Schema
+            jsonschema.validate(v, json.loads(js_schema), format_checker=jsonschema.draft7_format_checker)
+        except jsonschema.exceptions.ValidationError as err:
+            print(f'     JSON Schema: {err.message}')
 
 
 print('\nSerialized Data:\n----------------')
