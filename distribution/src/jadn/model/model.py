b87211806cb3af89e5ecfa5ca93804b8f7380c26
David Kemp <19152940+davaya@users.noreply.github.com>
2024-12-02 15:06:45 -0500

    Add Choice option for untagged union

---
+++ distribution/src/jadn/model/model.py
@@ -0,0 +1,5 @@
+class InformationModel:
+    def __init__(self):
+        self.encoding = []  # Encoding instances (serialization/encoding rules)
+        self.schema = []    # Schema instances (abstract schema packages)
+        self.format = []    # Format instances (semantic validation methods)
